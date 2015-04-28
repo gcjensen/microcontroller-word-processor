1d4c33885711d9d04dd43ab6f216fcbe6fcb9ef9
George <gj4g13@soton.ac.uk>
2015-04-28 15:53:09 +0100

    Holding down left or right now moves the selector faster

---
+++ _build/main.d
@@ -0,0 +1,16 @@
+_build/main.o: main.c os.h lcd/lcd.h rios/rios.h ruota/ruota.h fatfs/ff.h \
+ fatfs/integer.h fatfs/ffconf.h
+
+os.h:
+
+lcd/lcd.h:
+
+rios/rios.h:
+
+ruota/ruota.h:
+
+fatfs/ff.h:
+
+fatfs/integer.h:
+
+fatfs/ffconf.h:
--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 #include "os.h"
 
-//int blink(int);
-//int update_dial(int);
-//int collect_delta(int);
 int check_switches(int);
 char* uppercaseAlphabet = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ";
 char* alphabet = "[a]b c d e f g h i j k l m n o p q r s t u v w x y z \n _ < ";
@@ -10,12 +7,7 @@ int position = 1;
 char text[100];
 int textPosition = 0;
 
-
 FIL File;  						/* FAT File */
-
-volatile int8_t delta;
-
-
 
 void main(void) {
     os_init();
@@ -32,7 +24,6 @@ void main(void) {
 void printKeyboard(int direction) {
   clear_screen();
   display.y = display.height - 20;
-  //alphabet[position] = staticAlphabet[position];
   alphabet[position - 1] = ' ';
   alphabet[position + 1] = ' ';
   position = position + direction;
@@ -46,21 +37,17 @@ void printKeyboard(int direction) {
 
 int check_switches(int state) {
 
-	if (get_switch_press(_BV(SWN))) {
-			display_string("Up\n");
+  if (get_switch_rpt(_BV(SWE))) {
+       printKeyboard(2);
+  }
+
+  if (get_switch_rpt(_BV(SWW))) {
+       printKeyboard(-2);
 	}
 
-	if (get_switch_press(_BV(SWE))) {
-      printKeyboard(2);
-	}
+	if (get_switch_press(_BV(SWS))) {}
 
-	if (get_switch_press(_BV(SWS))) {
-			display_string("Down\n");
-	}
-
-	if (get_switch_press(_BV(SWW))) {
-      printKeyboard(-2);
-	}
+  if (get_switch_press(_BV(SWN))) {}
 
 	if (get_switch_short(_BV(SWC))) {
       if (alphabet[position] == '_') {
@@ -77,77 +64,5 @@ int check_switches(int state) {
       printKeyboard(0);
 	}
 
-  if (get_switch_long(_BV(SWC))) {
-      if (alphabet[position] == '_') {
-        text[textPosition] = ' ';
-        textPosition++;
-      } else if (alphabet[position] == '<') {
-        textPosition--;
-        text[textPosition] = ' ';
-      } else {
-        text[textPosition] = uppercaseAlphabet[position];
-        textPosition++;
-      }
-      display.y = 0;
-      printKeyboard(0);
-
-  }
-
-
-	// if (get_switch_rpt(_BV(SWN))) {
-	// 		display_string("[R] North\n");
-	// }
-  //
-	// if (get_switch_rpt(_BV(SWE))) {
-	// 		display_string("[R] East\n");
-	// }
-  //
-	// if (get_switch_rpt(_BV(SWS))) {
-	// 		display_string("[R] South\n");
-	// }
-  //
-	// if (get_switch_rpt(_BV(SWW))) {
-	// 		display_string("[R] West\n");
-	// }
-  //
-	// if (get_switch_rpt(SWN)) {
-	// 		display_string("[R] North\n");
-	// }
-  //
-  //
-	// if (get_switch_long(_BV(OS_CD))) {
-	// 	display_string("Detected SD card.\n");
-	// }
-
 	return state;
 }
-
-// ISR( TIMER0_COMPA_vect ) {
-//     static int8_t last;
-//     int8_t new, diff;
-//     uint8_t wheel;
-//
-//
-//     /*
-//       Scan rotary encoder
-//       ===================
-//       This is adapted from Peter Dannegger's code available at:
-//       http://www.mikrocontroller.net/articles/Drehgeber
-//     */
-//
-//     wheel = PINE;
-//     new = 0;
-//     if( wheel  & _BV(PE4) ) new = 3;
-//     if( wheel  & _BV(PE5) )
-//     new ^= 1;                  /* convert gray to binary */
-//     diff = last - new;         /* difference last - new  */
-//     if( diff & 1 ){            /* bit 0 = value (1) */
-//        last = new;                /* store new as next last  */
-//        delta = (diff & 2) - 1;   /* bit 1 = direction (+/-) */
-//        if (delta < 0) {
-//          printKeyboard(2);
-//        } else {
-//          printKeyboard(-2);
-//        }
-//     }
-// }
